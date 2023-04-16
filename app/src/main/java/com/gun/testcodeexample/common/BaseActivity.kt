472b55c84fe68638709f8a943f2df251f995c504
YoungGeon, Moon <my_gun5389@naver.com>
2023-04-17 04:52:35 +0900

    [기능추가] EditText 외부 클릭 시 키보드 내려가도록 수정, 키보드에 개행 버튼을 확인버튼으로 변경

--- app/src/main/java/com/gun/testcodeexample/common/BaseActivity.kt
+++ app/src/main/java/com/gun/testcodeexample/common/BaseActivity.kt
@@ -1,9 +1,12 @@
 package com.gun.testcodeexample.common
 
+import android.view.MotionEvent
 import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import com.gun.testcodeexample.api.retrofit.KeyboardUtils
 
-abstract class BaseActivity: AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity() {
     private var loadingBar: CustomLoadingBar? = null
 
     fun initLoadingBar(loadingBar: CustomLoadingBar) {
@@ -23,5 +26,23 @@ abstract class BaseActivity: AppCompatActivity() {
                 window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
             }
         }
+    }
+
+    private fun hideKeyboard() {
+        currentFocus?.let {
+            val inputManager = this.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            val iBinder = it.windowToken
+            val hideType = InputMethodManager.HIDE_NOT_ALWAYS
+
+            inputManager.hideSoftInputFromWindow(iBinder, hideType)
+            it.clearFocus()
+        }
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        if (KeyboardUtils.isTouchEditTextOutSide(currentFocus, ev.x.toInt(), ev.y.toInt())) {
+            hideKeyboard()
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
